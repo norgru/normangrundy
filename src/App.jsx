4231ba6dc75f11b7f31021ff90e38cb96d4adc94
Norman <grundy.norm@gmail.com>
2022-08-22 22:54:28 +0100

    yep

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  
+
+
+
+  return (
+    <div className="App">
+      <h1>Welcome to Norman's page - </h1>
+      <h2><a href="https://weather.normangrundy.com/">This</a></h2>
+    </div>
+  );
+}
+
+export default App;
