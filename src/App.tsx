85800a5e3535bfafb921a790bbec34852a35f764
Norman <grundy.norm@gmail.com>
2022-08-26 00:40:35 +0100

    first commit
    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-//import logo from './logo.svg';
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
-  
   return (
-
     <div className="App">
-      <h1>Welcome to Norman's page - </h1>
-      <h2><a href="https://weather.normangrundy.com/">This</a></h2>
+      <h3>Hello</h3>
     </div>
   );
 }
