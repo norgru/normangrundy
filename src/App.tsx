8ac4e7c7298f23b4c2cfb56a37ae57fd2cc311c4
Norman <grundy.norm@gmail.com>
2022-08-25 17:58:36 +0100

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 //import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   
   
-
-
-
   return (
+
     <div className="App">
       <h1>Welcome to Norman's page - </h1>
       <h2><a href="https://weather.normangrundy.com/">This</a></h2>
